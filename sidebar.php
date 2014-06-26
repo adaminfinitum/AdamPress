67ebf279614ac64f5aa154da5f963121446fb681
Adam Powell <adam@adaminfinitum.com>
2014-06-26 02:23:45 -0400

    Initial commit

---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<aside id="sidebar" class="small-12 large-4 columns">
+	<?php do_action('AdamPress_before_sidebar'); ?>
+	<?php dynamic_sidebar("sidebar-widgets"); ?>
+	<?php do_action('AdamPress_after_sidebar'); ?>
+</aside>
