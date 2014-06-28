dd039f5e96d0edfea84c9c6fec113401f9b1096a
Adam Powell <adam@adaminfinitum.com>
2014-06-28 03:31:18 -0400

    add rss feed and canonical options back to head via commenting lines out

--- library/cleanup.php
+++ library/cleanup.php
@@ -43,10 +43,10 @@ function cleanup_head() {
     remove_action( 'wp_head', 'rsd_link' );
 
     // Category feed links
-    remove_action( 'wp_head', 'feed_links_extra', 3 );
+    // remove_action( 'wp_head', 'feed_links_extra', 3 );
 
     // Post and comment feed links
-    remove_action( 'wp_head', 'feed_links', 2 );
+    // remove_action( 'wp_head', 'feed_links', 2 );
     
     // Windows Live Writer
     remove_action( 'wp_head', 'wlwmanifest_link' );
@@ -61,7 +61,7 @@ function cleanup_head() {
     remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
 
     // Canonical
-    remove_action('wp_head', 'rel_canonical', 10, 0 );
+    // remove_action('wp_head', 'rel_canonical', 10, 0 );
 
     // Shortlink
     remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
